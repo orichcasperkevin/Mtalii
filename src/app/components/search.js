4f643bc3ce711d12dba5af5dd6e69dcc8190190c
orichcasperkevin <orichcaspertheghost@gmail.com>
2024-03-06 11:45:29 +0000

    location search

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -1,13 +1,70 @@
+import React, { useState,useEffect } from 'react';
+
 export default function Search(){
-    return (
+  const [showLocations, setShowLocations] = useState(false);
+  const [locationSearch, setLocationSearch] = useState('');
+  const [locations, setLocations] = useState([]);
+
+  // Dummy list of locations for demonstration
+  const allLocations = [
+      'New York',
+      'Los Angeles',
+      'Chicago',
+      'Houston',
+      'Phoenix',
+      'Philadelphia',
+      'San Antonio',
+      'San Diego',
+      'Dallas',
+      'San Jose',
+    ];
+  
+  // Filter locations based on search query
+  const filteredLocations = allLocations.filter(location =>
+    location.toLowerCase().includes(locationSearch.toLowerCase())
+  );
+
+  const handleInputChange = event => {
+    setLocationSearch(event.target.value);
+    let searchValue = event.target.value
+    setShowLocations(searchValue.trim() !== '');
+  };
+
+  const handleLocationClick = location => {
+    // Do something with the selected location, e.g., set it in state or perform an action
+    console.log('Selected location:', location);
+    setShowLocations(false); // Hide locations dropdown after selection
+  };
+  return (
       <div class="flex items-center">
         <div class="grid min-h-[100px] w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
         <div class="flex w-full divide-x  row">
           <div
-            class="border font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-full shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
+            class="border relative font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-full shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
             type="button">
             Location  
-            <input type="text" placeholder="Where to?"></input>
+            <input className="p-3 border-none text-gray-500" type="text" placeholder="Where to?"
+             value={locationSearch}
+             onChange={handleInputChange}>              
+            </input>
+            {showLocations &&  (
+            <div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <ul className="p-3">
+                {filteredLocations.map((location, index) => (
+                  <li className="text-gray-900 rounded-full p-3 hover:bg-accent-1"
+                    key={index} onClick={() => handleLocationClick(location)}>
+                      {location}
+                  </li>                
+                ))}
+                </ul>
+            </div>
+            )}
+          </div>
+          <div
+            class="border grid font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
+            type="button">
+            Dates
+            <input type="text" placeholder="When ?"></input>
           </div>
           <div
             class="border grid font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import ProfileIcon from "./components/profile_icon";
 import Search from "./components/search";
 import SearchFilters from "./components/search_filters/search_filters";
 import CategoryFilter from './components/category_filter';
+
 export default function Home() {
   const [showFilters,setShowFilters] = useState(false)
 
@@ -69,17 +70,7 @@ export default function Home() {
       </div>
       <hr></hr>
       <div className='flex flex-row'>
-        <CategoryFilter className="w-3/4 border"/>
-        <span className='p-3 mt-3'>|</span>
-        <button
-            onClick={(e)=>{e.preventDefault();setShowFilters(true)}}
-            class="flex flex-row mt-3 select-none font-sans text-center transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-gray-900 rounded-full p-3 bg-gray-100 shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
-            type="button">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-            </svg>
-            <span className='ml-2'>More filters</span>
-        </button>
+        <CategoryFilter className="w-3/4 border"/>        
       </div>
     </main>
 
