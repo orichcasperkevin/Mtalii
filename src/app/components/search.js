4d9cb6dd79d3d3a4b7176d8cfca1fcc4b170c934
orichcasperkevin <orichcaspertheghost@gmail.com>
2024-03-07 11:16:48 +0000

    guest input

--- src/app/components/search.js
+++ src/app/components/search.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect } from 'react';
 import Datepicker from "react-tailwindcss-datepicker";
+import GuestInput from './search_filters/guests';
 
 export default function Search(){
   const [showLocations, setShowLocations] = useState(false);
@@ -7,7 +8,7 @@ export default function Search(){
   const [dates, setDates] = useState({ 
     startDate: null, 
     endDate: null 
-    }); 
+  }); 
   
   // Dummy list of locations for demonstration
   const allLocations = [
@@ -36,28 +37,30 @@ export default function Search(){
 
   const handleLocationClick = location => {
     // Do something with the selected location, e.g., set it in state or perform an action
-    console.log('Selected location:', location);
+    setLocationSearch(location)
     setShowLocations(false); // Hide locations dropdown after selection
   };
 
   const handleDatesChange = (newValue) => {
     console.log("newValue:", newValue);
     setDates(newValue); 
-    } 
+  } 
+
   return (
       <div class="flex items-center">
         <div class="grid min-h-[100px] w-full place-items-center overflow-x-scroll rounded-lg lg:overflow-visible">
         <div class="flex w-full divide-x  row">
+          {/* location */}
           <div
             class="border relative grid font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-full shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
             type="button">
             Location  
-              <input className="border-none text-gray-500" type="text" placeholder="Where to?"
+              <input className="p-1 border-none text-gray-500" type="text" placeholder="Where to?"
               value={locationSearch}
               onChange={handleInputChange}>              
               </input>
               {showLocations &&  (
-              <div className="absolute right-0 top-20 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="absolute right-0 top-20 z-20 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                   <ul className="p-3">
                   {filteredLocations.map((location, index) => (
                     <li className="text-gray-900 rounded-full p-3 hover:bg-accent-1"
@@ -69,6 +72,7 @@ export default function Search(){
               </div>
               )}
           </div>
+          {/* Dates */}
           <div
             class="border grid font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] block w-full rounded-r-none border-r-0"
             type="button">
@@ -81,15 +85,18 @@ export default function Search(){
               showShortcuts={true} 
               placeholder='When ?'
             /> 
-          </div>          
-          <div
-            class="flex flex-row border font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] w-full rounded-full rounded-l-none border-r-0"
+          </div>      
+          {/* Guests     */}
+          <div class="relative flex flex-row border font-bold transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 shadow-md shadow-gray-900/10 focus:shadow-none active:opacity-[0.85] w-full rounded-full rounded-l-none border-r-0"
             type="button">
             <div>
               Guests
-              <input type="text" placeholder="Who's coming?"></input>
+              <input type="text" className='p-1' placeholder="Who's coming?"></input>
+              <div className="absolute right-0 top-20 z-20 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <GuestInput/>
+              </div>
             </div>   
-            <button className="bg-accent-1 rounded-full p-2">                
+            <button className="ml-2 bg-accent-1 rounded-full p-2">                
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                 </svg>
---
+++ src/app/components/search_filters/guests.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const GuestInput = () => {
+    const [adults, setAdults] = useState(0);
+    const [children, setChildren] = useState(0);
+    const [infants, setInfants] = useState(0);
+    const [pets, setPets] = useState(0);
+
+    const handleIncrement = (setter) => {
+        setter((prevCount) => prevCount + 1);
+    };
+
+    const handleDecrement = (setter) => {
+        setter((prevCount) => (prevCount > 0 ? prevCount - 1 : 0));
+    };
+
+    return (
+        <div className="p-3 flex flex-col space-y-4">
+            <div className="flex justify-between">
+                <span className="p-2">Adults:</span>
+                <div>
+                    <button onClick={() => handleDecrement(setAdults)}  className="bg-accent-1 p-2 rounded-full">
+                        -
+                    </button>
+                    <input type="number" className="input-field w-12 text-center" value={adults} readOnly />
+                    <button onClick={() => handleIncrement(setAdults)} className="bg-accent-1 p-2 rounded-full">
+                        +
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <span className="p-2">Children:</span>
+                <div>
+                    <button onClick={() => handleDecrement(setChildren)}  className="bg-accent-1 p-2 rounded-full">
+                        -
+                    </button>
+                    <input type="number" className="input-field w-12 text-center" value={children} readOnly />
+                    <button onClick={() => handleIncrement(setChildren)}  className="bg-accent-1 p-2 rounded-full">
+                        +
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <span className="p-2">Infants:</span>
+                <div>
+                    <button onClick={() => handleDecrement(setInfants)}  className="bg-accent-1 p-2 rounded-full">
+                        -
+                    </button>
+                    <input type="number" className="input-field w-12 text-center" value={infants} readOnly />
+                    <button onClick={() => handleIncrement(setInfants)}  className="bg-accent-1 p-2 rounded-full">
+                        +
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-between">
+                <span className="p-2">Pets:</span>
+                <div>
+                    <button onClick={() => handleDecrement(setPets)}  className="bg-accent-1 p-2 rounded-full">
+                        -
+                    </button>
+                    <input type="number" className="input-field w-12 text-center" value={pets} readOnly />
+                    <button onClick={() => handleIncrement(setPets)}  className="bg-accent-1 p-2 rounded-full">
+                        +
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default GuestInput;
