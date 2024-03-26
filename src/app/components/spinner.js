a42a8c437f38d249efab8f75d1a9d98c01e30757
orichcasperkevin <orichcaspertheghost@gmail.com>
2024-03-26 12:26:55 +0000

    spinner

--- src/app/components/spinner.js
+++ src/app/components/spinner.js
@@ -1,3 +1,3 @@
 export default function Spinner(){
-    return <div class="border-accent-1 h-6 w-6 animate-spin rounded-full border-4 border-t-secondary-1" />
+    return <div className="border-accent-1 h-6 w-6 animate-spin rounded-full border-4 border-t-secondary-1" />
 } 
